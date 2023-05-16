b7be443f73e42f9ff13ef3aaef8bd395a05d7485
Keyzan7 <94049327+Keyzan7-7@users.noreply.github.com>
2023-05-16 13:22:18 +0200

    Add files via upload

---
+++ GreenBallA/src/controlador/FMXLMainController.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package controlador;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author keyza
+ */
+public class FMXLMainController implements Initializable {
+
+    @FXML
+    private TextField CampoUsuario;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
