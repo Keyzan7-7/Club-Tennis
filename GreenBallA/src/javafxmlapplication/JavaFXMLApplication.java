301ccb6dd76e48ff6765a1bfdf5501df7e7538ea
Keyzan7 <94049327+Keyzan7-7@users.noreply.github.com>
2023-05-17 15:56:50 +0200

    Add files via upload

--- GreenBallA/src/javafxmlapplication/JavaFXMLApplication.java
+++ GreenBallA/src/javafxmlapplication/JavaFXMLApplication.java
@@ -4,38 +4,34 @@
  * and open the template in the editor.
  */
 package javafxmlapplication;
-
-import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import model.Club;
+import static model.Club.*;
+
 
 
 public class JavaFXMLApplication extends Application {
-    
-    private static Scene scene;
-    
-    public static void setRoot(Parent root) throws IOException{
-        scene.setRoot(root);
-    }
     
     @Override
     public void start(Stage stage) throws Exception {
         //======================================================================
         // 1- creación del grafo de escena a partir del fichero FXML
-        FXMLLoader loader= new  FXMLLoader(getClass().getResource("/vista/Registrarse.fxml"));
+        FXMLLoader loader= new  FXMLLoader(getClass().getResource("/vista/VistaMain_1.fxml"));
         Parent root = loader.load();
         //======================================================================
         // 2- creación de la escena con el nodo raiz del grafo de escena
-        scene = new Scene(root);
+        Scene scene = new Scene(root);
         //======================================================================
         // 3- asiganación de la escena al Stage que recibe el metodo 
         //     - configuracion del stage
         //     - se muestra el stage de manera no modal mediante el metodo show()
         stage.setScene(scene);
         stage.setTitle("GreenBall");
+        Club club = Club.getInstance();
         stage.show();
     }
 
